0da963fe53e47752d661a49298d105462b1abc34
ngphctn14 <79503417+ngphctn14@users.noreply.github.com>
2023-02-28 17:04:56 +0700

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
-int draw;
-int owins=0;
-int xwins=0;
+int owins=0, xwins=0, draw=0;
 
 int isValidInput(int, int, char);
 int isValidTurn(int, char);
-int isValidTurn1(int, int, char[6][6]);
+int isValidMove(int, int, char[6][6]);
 void printBoard(char[6][6]);
 void result(char[6][6]);
 void posChanger(int*, int*);
 void setBoard(char[6][6]);
+void winCond(int, char);
 
 int main() {
-    char board[6][6];
-    int col, row, stop=0;
-    char piece;
-    int count=0;
+    char board[6][6], piece;
+    int col, row, stop=0, count=0;
     setBoard(board);
     printf("\n================ Tic Tac Toe ================\n\n");
     do {
         printBoard(board);
         if(count>4)
             result(board);
-        if(owins || xwins) {
+        if(owins || xwins)
             break;
-        }
         if(count==9) {
             draw=1;
             break;
@@ -39,7 +35,7 @@ int main() {
                 break;
             }
             printf("\n");
-        } while(!isValidInput(row, col, piece) || !isValidTurn(count, piece) || !isValidTurn1(row, col, board));
+        } while(!isValidInput(row, col, piece) || !isValidTurn(count, piece) || !isValidMove(row, col, board));
         count++;
         if(stop) {
             printf("\nThe game has been stopped.\n");
@@ -47,7 +43,7 @@ int main() {
         }
         posChanger(&col, &row);
         board[row][col]=piece;
-    } while(1);
+    } while(69/*funny number*/);
     if(owins)
         printf("\nO won\nX lost\n");
     else if(xwins)
@@ -79,18 +75,21 @@ int isValidInput(int row, int col, char piece) {
 }
 
 int isValidTurn(int count, char piece) {
-    if(count%2==0 && piece=='X')
-        return 1;
-    else if(count%2==1 && piece=='O')
-        return 1;
-    return 0;
+    return ((count%2==0 && piece=='X') || (count%2==1 && piece=='O'));
 }
 
-int isValidTurn1(int row, int col, char a[6][6]) {
+int isValidMove(int row, int col, char a[6][6]) {
     posChanger(&col, &row);
-    if(a[row][col]=='O' || a[row][col]=='X')
-        return 0;
-    return 1;
+    return !(a[row][col]=='O' || a[row][col]=='X');
+}
+
+void printBoard(char a[6][6]) {
+    int i, j;
+    for(i=0; i<5; i++) {
+        for(j=0; j<5; j++)
+            printf("%c", a[i][j]);
+        printf("\n");
+    }
 }
 
 void posChanger(int *row, int *col) {
@@ -104,13 +103,15 @@ void posChanger(int *row, int *col) {
         (*col)++;
 }
 
-void printBoard(char a[6][6]) {
-    int i, j;
-    for(i=0; i<5; i++) {
-        for(j=0; j<5; j++) {
-            printf("%c", a[i][j]);
+void winCond(int count, char piece) {
+    if(count==2) {
+        if(piece=='O') {
+            owins=1;
+            return;
+        } else {
+            xwins=1;
+            return;
         }
-        printf("\n");
     }
 }
 
@@ -119,65 +120,37 @@ void result(char a[6][6]) {
     for(i=0; i<3; i+=2) {
         count=0;
         for(j=0; j<3; j+=2) {
-            if(a[i][j]==a[i][j+2] && (a[i][j]=='O' || a[i][j]=='X')) {
+            if(a[i][j]==a[i][j+2] && (a[i][j]=='O' || a[i][j]=='X'))
                 count++;
-            }
-            if(count==2) {
-                if(a[i][j]=='O') {
-                    owins=1;
-                    return;
-                } else {
-                    xwins=1;
-                    return;
-                }
-            }
+            winCond(count, a[i][j]);
+            if(owins || xwins)
+                return;
         }
     }
     for(j=0; j<3; j+=2) {
         count=0;
         for(i=0; i<3; i+=2) {
-            if(a[i][j]==a[i+2][j] && (a[i][j]=='O' || a[i][j]=='X')) {
+            if(a[i][j]==a[i+2][j] && (a[i][j]=='O' || a[i][j]=='X'))
                 count++;
-            }
-            if(count==2) {
-                if(a[i][j]=='O') {
-                    owins=1;
-                    return;
-                } else {
-                    xwins=1;
-                    return;
-                }
-            }
+            winCond(count, a[i][j]);
+            if(owins || xwins)
+                return;
         }
     }
     count=0;
     for(i=0, j=0; i<3; i+=2, j+=2) {
-        if(a[i][j]==a[i+2][j+2] && (a[i][j]=='O' || a[i][j]=='X')) {
+        if(a[i][j]==a[i+2][j+2] && (a[i][j]=='O' || a[i][j]=='X'))
             count++;
-        }
-        if(count==2) {
-            if(a[i][j]=='O') {
-                owins=1;
-                return;
-            } else {
-                xwins=1;
-                return;
-            }
-        }
+        winCond(count, a[i][j]);
+        if(owins || xwins)
+            return;
     }
     count=0;
     for(i=0, j=4; i<3; i+=2, j-=2) {
-        if(a[i][j]==a[i+2][j-2] && (a[i][j]=='O' || a[i][j]=='X')) {
+        if(a[i][j]==a[i+2][j-2] && (a[i][j]=='O' || a[i][j]=='X'))
             count++;
-        }
-        if(count==2) {
-            if(a[i][j]=='O') {
-                owins=1;
-                return;
-            } else {
-                xwins=1;
-                return;
-            }
-        }
+        winCond(count, a[i][j]);
+        if(owins || xwins)
+            return;
     }
 }
